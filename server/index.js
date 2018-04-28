3d56e8a2b485cce7077b2bc16c0cc509289373ab
Matt <dev@mattgween.com>
2018-04-28 00:20:04 -0400

    Removed app.listen code on the server

--- server/index.js
+++ server/index.js
@@ -87,49 +87,7 @@ app.post('/new-user', (req, res) => {
   });
 });
 
-// db.collection('prizes').insertMany([
-//   {
-//     name: 'Lorem Ipsum',
-//     description: 'Quisque velit nisi, pretium ut lacinia in, elementum id enim. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.',
-//     image_url: 'http://via.placeholder.com/500x300',
-//     quantity: 30
-//   },
-//   {
-//     name: 'Quisque Velit',
-//     description: 'Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.',
-//     image_url: 'http://via.placeholder.com/500x300',
-//     quantity: 25
-//   },
-//   {
-//     name: 'Libero Malesuada',
-//     description: 'Donec sollicitudin molestie malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Sed porttitor lectus nibh.',
-//     image_url: 'http://via.placeholder.com/500x300',
-//     quantity: 20
-//   },
-//   {
-//     name: 'Tempus Convallis',
-//     description: 'Donec rutrum congue leo eget malesuada. Pellentesque in ipsum id orci porta dapibus. Nulla porttitor accumsan tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.',
-//     image_url: 'http://via.placeholder.com/500x300',
-//     quantity: 15
-//   },
-//   {
-//     name: 'Donec Rutrum',
-//     description: 'Cras ultricies ligula sed magna dictum porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat.',
-//     image_url: 'http://via.placeholder.com/500x300',
-//     quantity: 10
-//   },
-//   {
-//     name: 'Orci Porta',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
-//     image_url: 'http://via.placeholder.com/500x300',
-//     quantity: 5
-//   }
-// ], (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
+// const port = 3636;
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}`);
 // });
-
-const port = 3636;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
